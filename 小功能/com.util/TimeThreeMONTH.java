6e8262814003dc201d64fbfe620f9a0ef84ed1d1
ex_lianggang <ex_lianggang@partner.midea.com>
2020-03-17 10:10:11 +0800

    线程笔记

--- 小功能/com.util/TimeThreeMONTH.java
+++ 小功能/com.util/TimeThreeMONTH.java
@@ -1,18 +1,20 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * 功能三个月之内
+ */
 public class TimeThreeMONTH {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date =new Date();
+        System.out.println(TimeThreeMONTH.checkThreeMonth(format.parse("2020-10-01 00:00:00"),format.parse("2020-04-01 00:00:00")));
+    }
+    public  static boolean checkThreeMonth(Date startTime,Date endTime) throws ParseException {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        int month =calendar.get(Calendar.MONTH) +3;
-        if(month>8){
-
-        }
-
+        calendar.setTime(startTime);
+        int month =calendar.get(Calendar.MONTH);
         calendar.set(Calendar.DATE, 1);
         // 时
         calendar.set(Calendar.HOUR_OF_DAY, 0);
@@ -22,9 +24,8 @@ public class TimeThreeMONTH {
         calendar.set(Calendar.SECOND, 0);
         // 毫秒
         calendar.set(Calendar.MILLISECOND, 0);
-//        Date emdTime =
-//        System.out.println(format.format(calendar.getTime()));
-//        System.out.println(month);
-
+        calendar.add(Calendar.MONTH,3);//三个月内
+        calendar.add(Calendar.MILLISECOND, -1);
+        return calendar.getTime().compareTo(endTime)==1?true:false;
     }
 }
